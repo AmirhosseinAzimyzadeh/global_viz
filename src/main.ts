094b7b7933e3b5cd69199728ff920c97c91bef30
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2023-02-05 21:15:41 +0330

    add sphere

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,42 @@
 import * as THREE from 'three';
+import textureGenerator from './textureGenerator';
 
 export {}
 
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <canvas id="canvas"></canvas>
-    GLOB VIZ
-  </div>
-`
+  <canvas
+    id="canvas"
+    style="max-width: 100%; height: 100%;"
+  ></canvas>
+`;
+
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
 
 const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
-const renderer = new THREE.WebGLRenderer({ canvas });
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
 const fov = 75;
-const aspect = 2;  // the canvas default
+const aspect = WIDTH / HEIGHT;
 const near = 0.1;
 const far = 5;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color('white')
 
-const boxWidth = 1;
-const boxHeight = 1;
-const boxDepth = 1;
-const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const geometry = new THREE.SphereGeometry(1, 32, 32);
 
 const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });  // greenish blue
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 renderer.render(scene, camera);
+
+textureGenerator();
