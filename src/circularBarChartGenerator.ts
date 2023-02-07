41762a26532cfc8c1ce65c174e1369037d455ad3
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2023-02-07 11:21:56 +0330

    use state and add background bar

--- src/circularBarChartGenerator.ts
+++ src/circularBarChartGenerator.ts
@@ -1,17 +1,14 @@
 import * as d3 from 'd3';
-
-const testData = Array.from(Array(255))
-  .map((_, i) => ({ name: `Test${i}`, value: i }));
-
-// shuffle array
-// for (let i = testData.length - 1; i > 0; i--) {
-//   const j = Math.floor(Math.random() * (i + 1));
-//   [testData[i], testData[j]] = [testData[j], testData[i]];
-// }
-
-console.log(testData);
+import state from './State';
 
 export default function circularBarChartGenerator() {
+  const worldMap = state.getWorldMap()!;
+  const data = worldMap.features.map((f) => ({
+    name: f.properties.ADMIN,
+    value: f.properties.population,
+    code: f.properties.ISO_A3,
+  }));
+
   const margin = {
     top: 20,
     right: 20,
@@ -22,7 +19,7 @@ export default function circularBarChartGenerator() {
   const width = window.innerHeight - margin.left - margin.right;
   const height = window.innerHeight - margin.top - margin.bottom;
 
-  const innerRadius = 0.22 * width;
+  const innerRadius = 0.25 * height;
   const outerRadius = 0.4 * width;
 
   const svg = d3.select('#chart')
@@ -31,24 +28,55 @@ export default function circularBarChartGenerator() {
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
-    .attr('transform', `translate(${width / 2 + margin.left},${height / 2 + margin.top})`);
+    .attr('transform', `translate(${width / 2 + margin.left},${height / 2 + margin.top})`)
+
 
   const x = d3.scaleBand()
     .range([0, 2 * Math.PI])
     .align(0)
-    .domain(testData.map((d) => d.name));
+    .domain(data.map((d) => d.name));
   
   const y = d3.scaleRadial()
     .range([innerRadius, outerRadius])
-    .domain([0, 255]);
+    .domain([0, state.getMaxPopulation()]);
+
+  svg.append('g')
+  .selectAll('path')
+  .data(data)
+  .enter()
+  .append('path')
+    .attr('fill', 'rgba(0,0,0,0.02)')
+    .attr('d', d3.arc<{ name: string, value: number }>() 
+        .innerRadius((d) => y(d.value))
+        .outerRadius(outerRadius)
+        .startAngle((d) =>  Number(x(d.name)))
+        .endAngle((d) => Number(x(d.name)) + x.bandwidth())
+        .padAngle(0.01)
+        .padRadius(innerRadius)
+    )
+  .on('mouseover', (e: MouseEvent, data) => {
+    if (e.target === null) return;
+    d3.select(e.target as SVGPathElement).attr('fill', 'red');
+    state.setSelectedCountry({
+      ADMIN: data.name,
+      ISO_A3: data.code,
+      population: data.value,
+    });
+  })
+  .on('mouseout', (e: MouseEvent) => {
+    if (e.target === null) return;
+    state.setSelectedCountry(null);
+    d3.select(e.target as SVGPathElement).attr('fill', 'rgba(0,0,0,0.02)');
+  });
+  
   
   svg.append('g')
   .selectAll('path')
-  .data(testData)
+  .data(data)
   .enter()
   .append('path')
-    .attr('fill', (d: any) => d3.interpolateSpectral(d['value'] / 255))
-    .attr('d', d3.arc<{ name: string, value: number }>()     // imagine your doing a part of a donut plot
+    .attr('fill', (d: any) => d3.interpolateSpectral(d['value'] / state.getMaxPopulation()))
+    .attr('d', d3.arc<{ name: string, value: number }>() 
         .innerRadius(innerRadius)
         .outerRadius((d) => y(d.value))
         .startAngle((d) =>  Number(x(d.name)))
@@ -56,21 +84,11 @@ export default function circularBarChartGenerator() {
         .padAngle(0.001)
         .padRadius(innerRadius)
     )
-  .on('mouseover', (e: MouseEvent, data) => {
-    if (e.target === null) return;
-    d3.select(e.target as SVGPathElement).attr('fill', 'red');
-    // e.target.setAttribute('fill', 'red');
-    console.log(data);
-  })
-  .on('mouseout', (e: MouseEvent, data) => {
-    if (e.target === null) return;
-    d3.select(e.target as SVGPathElement).attr('fill', (d: any) => d3.interpolateSpectral(d['value'] / 255));
-  });
 
 
   svg.append('g')
   .selectAll('g')
-  .data(testData)
+  .data(data)
   .enter()
   .append('g')
     .attr('text-anchor', (d) => (Number(x(d.name)) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? 'end' : 'start')
@@ -81,5 +99,5 @@ export default function circularBarChartGenerator() {
     .style('font-size', '11px')
     .attr('alignment-baseline', 'middle')
     .style('font-size', '5px')
-      
+    .style('cursor', 'default')
 }
