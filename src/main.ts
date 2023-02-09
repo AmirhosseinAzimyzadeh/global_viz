5fd58abddc40781406cb693ced429c9d5a6e8fe7
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2023-02-09 22:27:31 +0330

    fix sphere rotation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
+import * as d3 from 'd3';
 import circularBarChartGenerator from './circularBarChartGenerator';
+import Debounce from './Debounce';
 import state from './State';
 import textureGenerator from './textureGenerator';
 
@@ -12,6 +14,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   ></canvas>
 `;
 
+const debounce = new Debounce();
+
 async function initialize() {
   await state.init();
   let WIDTH = window.innerWidth;
@@ -22,10 +26,10 @@ async function initialize() {
   canvas.height = HEIGHT;
   const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
-  const fov = 90;
+  const fov = 2;
   let aspect = WIDTH / HEIGHT;
   const near = 0.01;
-  const far = 100;
+  const far = 9000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
   camera.position.z = 2;
@@ -33,21 +37,31 @@ async function initialize() {
   const scene = new THREE.Scene();
   scene.background = new THREE.Color('rgb(245, 245, 245)');
 
-  const geometry = new THREE.SphereGeometry(0.85, 30, 30);
+  const geometry = new THREE.SphereGeometry(10, 50, 50);
 
-  const textureURI = await textureGenerator();
+  const textureURI = await textureGenerator(null);
 
-  const texture = new THREE.TextureLoader().load(textureURI);
-  const material = new THREE.MeshPhongMaterial({ map: texture });
+  const initialTexture = new THREE.TextureLoader().load(textureURI);
+  const material = new THREE.MeshPhongMaterial({
+    map: initialTexture,
+    opacity: 0,
+    color: 'rgb(245, 245, 245)'
+  });
+  material.needsUpdate = true;
+  material.precision = 'highp';
 
   const sphere = new THREE.Mesh(geometry, material);
-  sphere.rotation.y -= 2;
+  sphere.position.z = -1000;
+  sphere.material.needsUpdate = true;
+
+  // rotate to 0,0 coordinate
+  sphere.rotation.y = -Math.PI / 2;
 
   scene.add(sphere);
 
   // light
   const light = new THREE.SpotLight('#fff', 0.5);
-  light.position.set(-2, 2, 5);
+  light.position.set(-1, 2, 2);
   scene.add(light);
 
   // ambient light
@@ -58,18 +72,57 @@ async function initialize() {
     sphere,
     renderer,
     camera,
+    material,
     scene
   }
 }
 
 
 function main() {
-    initialize().then(({ sphere, renderer, scene, camera }) => {
+    initialize().then(({
+      sphere,
+      renderer,
+      scene,
+      camera,
+    }) => {
     circularBarChartGenerator();
-    const animate = () => {
+
+    state.addChangeListener(() => {
+      debounce.use(() => {
+        textureGenerator(state.getSelectedCountry()!?.ISO_A3).then((newTex) => {
+          new THREE.TextureLoader()
+            .load(
+              newTex,
+              (t) => {
+                sphere.material.map = t;
+              }
+            );
+        });
+      })
+    })
+
+    let yRotateTarget = (-Math.PI / 2);
+    let xRotateTarget = 0;
+
+    const animate: () => void = () => {
       requestAnimationFrame(animate);
-      // sphere.rotation.x += 0.01;
-      sphere.rotation.y += 0.002;
+
+      const center = state.getSelectedCountryCenter();
+      if (center) {
+        // rotate to center
+        const [long, lat] = center;
+        yRotateTarget = (-Math.PI / 2) - (((long) * Math.PI) / 180);
+        xRotateTarget = (((lat) * Math.PI) / 90) / 2;
+        sphere.rotation.x = xRotateTarget;
+        sphere.rotation.y = yRotateTarget;
+
+        // cubic bezier animation to the rotate target
+      } else {
+        sphere.rotation.y += 0.001;
+        xRotateTarget = 0;
+        yRotateTarget = (-Math.PI / 2);
+      }
+
       renderer.render(scene, camera);
     }
     animate();
